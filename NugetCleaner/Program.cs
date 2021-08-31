2fd987d6088e55ae088fd75f0775347a4f68b415
Steve <hez2010@outlook.com>
2021-08-31 11:02:48 +0800

    Meh

--- NugetCleaner/Program.cs
+++ NugetCleaner/Program.cs
@@ -1,4 +1,7 @@
-﻿using NuGet.Versioning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NuGet.Versioning;
 using Spectre.Console;
 
 static long CalculateDirectorySize(string directory)
